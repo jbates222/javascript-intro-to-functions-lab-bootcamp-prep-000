b87a1b9a51911a245c95ea349bbd25f6851d1acc
Jackson Bates <jkbates222@gmail.com>
2018-09-11 00:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string) {
   return string.toUpperCase()
 }
+function whisker(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log("HELLO!")
+}
+function logWhisper(string) {
+  console.log("hello")
+}
